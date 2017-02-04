f3b351920675a2708d3a8e80461d9ca4d9ff6cd4
mark <iamwh1temark@outlook.com>
2017-02-04 18:12:40 +0800

    update

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,13 +1,15 @@
-package recfun
+// package recfun
 
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
+    println(balance(":-)".toList))
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+     println(balance(":-)"))
   }
 
   /**
@@ -18,17 +20,33 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean ={
-    def test(n:Int,chars:List[Char]): Boolean =
-      if (n < 0) false else
-        if (chars.isEmpty && n == 0) true else
-          if (chars.head == "(") test(n+1,chars.tail) else
-            if (chars.head == ")") test(n-1,chars.tail) else
-              test(n,chars.tail)
-    test(0,chars)
+  // def balance(chars: List[Char],counter:Int=0): Boolean ={
+  //   def count(char:Char)  =
+  //     if ( char == "(" ) 1
+  //     else if ( char == ")" ) -1
+  //     else 0
+
+  //   if (chars.isEmpty)
+  //     counter == 0
+  //   else if (counter<0) false
+  //   else balance(chars.tail,count(chars.head)+counter)
+  // }
+
+  def balance(chars: List[Char]): Boolean = {
+    def process(count: Int, rest: List[Char]): Boolean =
+      if (count < 0) false else
+        if (rest.isEmpty) (count == 0) else
+          if (rest.head == '(') process(count+1, rest.tail) else
+            if (rest.head == ')') process(count-1, rest.tail) else process(count, rest.tail)
+
+    process(0, chars)
   }
   /**
    * Exercise 3
    */
-  // def countChange(money: Int, coins: List[Int]): Int =
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (coins.isEmpty) 0
+    else if (money == coins.head) 1 + countChange(money,coins.tail)
+    else if (money < coins.head) countChange(money,coins.tail)
+    else countChange(money - coins.head,coins) + countChange(money,coins.tail)
   }
