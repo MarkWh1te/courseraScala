5e3746a2bcf68bb49109f081c82e8cc11cb9ef79
mark <iamwh1temark@outlook.com>
2017-02-05 11:19:51 +0800

    ipdate

---
+++ improvisation/notes.scala
@@ -0,0 +1,13 @@
+object notes{
+  def main(args:Array[String])=
+    // println(product(x => x+1,1,4))
+    println(factorial(10))
+
+  def product(f:Int=>Int,a:Int,b:Int):Int =
+    if (a > b) 1
+    else f(a) * product(f,a+1,b)
+
+  def factorial(a:Int):Int =
+    product(x=>x,1,a)
+
+}
