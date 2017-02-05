bff7c9e4ec597713623f0d97f3dfbc66aa9b4b79
mark <iamwh1temark@outlook.com>
2017-02-05 09:04:45 +0800

    update

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,36 +1,20 @@
-// package recfun
+package recfun
+import common._
 
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
-    println(balance(":-)".toList))
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
-     println(balance(":-)"))
   }
 
-  /**
-   * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int =
-    if (c == 0 || c == r) 1 else pascal(c-1,r)+pascal(c,r-1)
-  /**
-   * Exercise 2
-   */
-  // def balance(chars: List[Char],counter:Int=0): Boolean ={
-  //   def count(char:Char)  =
-  //     if ( char == "(" ) 1
-  //     else if ( char == ")" ) -1
-  //     else 0
-
-  //   if (chars.isEmpty)
-  //     counter == 0
-  //   else if (counter<0) false
-  //   else balance(chars.tail,count(chars.head)+counter)
-  // }
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1 else
+      pascal(c - 1, r - 1) + pascal(c, r - 1);
+  }
 
   def balance(chars: List[Char]): Boolean = {
     def process(count: Int, rest: List[Char]): Boolean =
@@ -41,12 +25,12 @@ object Main {
 
     process(0, chars)
   }
-  /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int =
-    if (coins.isEmpty) 0
-    else if (money == coins.head) 1 + countChange(money,coins.tail)
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(coins.isEmpty) 0
     else if (money < coins.head) countChange(money,coins.tail)
-    else countChange(money - coins.head,coins) + countChange(money,coins.tail)
+    else if (money == coins.head) 1 + countChange(money,coins.tail)
+    else countChange(money-coins.head,coins) + countChange(money,coins.tail)
   }
+
+}
