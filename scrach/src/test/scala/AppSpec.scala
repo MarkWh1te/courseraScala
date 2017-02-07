368d6a653778e4a0b1973a9d70047306c3950e9d
mark <iamwh1temark@outlook.com>
2017-02-07 18:10:19 +0800

    update

---
+++ scrach/src/test/scala/AppSpec.scala
@@ -0,0 +1,19 @@
+package markwh1te.scrach
+
+import org.scalatest.{ Matchers, PropSpec }
+import org.scalatest.prop._
+
+class CheckSpec
+    extends PropSpec
+    with GeneratorDrivenPropertyChecks
+    with Matchers {
+
+  property("Addition and multiplication are related") {
+    forAll { (x: Int) ⇒
+      whenever(x > 0) {
+        x * 2 should be(x + x)
+      }
+    }
+  }
+
+}
