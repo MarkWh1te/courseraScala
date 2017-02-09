b54c4a1cc4772cb55b65985db818cfbadb56b441
mark <iamwh1temark@outlook.com>
2017-02-09 19:03:14 +0800

    f

--- scrach/src/main/scala/pattern_matching.scala
+++
@@ -1,9 +0,0 @@
-package week4
-
-object PatternMatching {
-  def matchTest(x: Any): Any = x match {
-    case 1          ⇒ "this is one !"
-    case x: Int     ⇒ "this is a Int"
-    case x: String  ⇒ x
-    case x: Boolean :q
-        ⇒ IFu (X) "TRUEUUUUUuuuukkkkkkk  
