9aadc2e98cd78a1df901a7690c0d123abf469d24
mark <iamwh1temark@outlook.com>
2017-02-09 09:55:15 +0800

    add  one instance

--- scrach/src/main/scala/pattern_matching.scala
+++ scrach/src/main/scala/pattern_matching.scala
@@ -2,12 +2,14 @@ package week4
 
 object PatternMatching {
   def matchTest(x: Any): Any = x match {
+    case 1          ⇒ "this is one !"
     case x: Int     ⇒ "this is a Int"
     case x: String  ⇒ x
     case x: Boolean ⇒ if (x) "true" else "flase"
   }
   def main(args: Array[String]): Unit = {
     println(matchTest(1))
+    println(matchTest(2))
     println(matchTest("this is a test"))
     println(matchTest(true))
   }
