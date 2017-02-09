0c9647d641440d9be5015bb7dd3eb4bcc82a2806
mark <iamwh1temark@outlook.com>
2017-02-09 11:19:57 +0800

    add insertion sort and prepare week4 homework

---
+++ scrach/src/main/scala/insertion_sort.scala
@@ -0,0 +1,18 @@
+package week4
+
+object InsertionSort {
+  def issort(xs: List[Int]): List[Int] = xs match {
+    case List() ⇒ List()
+    case y :: ys ⇒ insert(y, issort(ys))
+  }
+  def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+    case List() ⇒ List(x)
+    case y :: ys ⇒ if (x <= y) x :: xs else y :: insert(x, ys)
+  }
+  def main(args: Array[String]): Unit = {
+    val testList = List(7, 2, 4, 56, 6)
+    val soredList = issort(testList)
+    println(testList)
+    println(soredList)
+  }
+}
