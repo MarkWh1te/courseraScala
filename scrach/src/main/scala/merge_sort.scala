8d16b6f88a1b6cb58c48c36a0abe702a12942f77
mark <iamwh1temark@outlook.com>
2017-02-10 16:41:57 +0800

    update

---
+++ scrach/src/main/scala/merge_sort.scala
@@ -0,0 +1,16 @@
+package week5
+
+
+object MergeSort {
+  def main(args:Array[String]):Unit ={
+    val testList = List(7, 2, 4, 56, 6)
+    val soredList = issort(testList)
+  }
+  def msort(list:List[Int]):List[Int]={
+    val n = list.length/2
+    if (n==0) list
+    else{
+
+    }
+  }
+}
